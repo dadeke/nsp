08e9a424c1d3e138cc9600a0ee12e371fc9dcdc2
Deividson Damasio <deividsondamasio@yahoo.com.br>
2021-03-09 17:35:21 -0300

    Add request answer

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import UserController from './controllers/UserController';
 import SurveyController from './controllers/SurveyController';
 import SendMailController from './controllers/SendMailController';
+import AnswerController from './controllers/AnswerController';
 
 const router = Router();
 const userController = new UserController();
 const surveyController = new SurveyController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 router.post('/users', userController.create);
 
@@ -15,5 +17,7 @@ router.get('/surveys', surveyController.show);
 router.post('/surveys', surveyController.create);
 
 router.post('/sendmail', sendMailController.execute);
+
+router.get('/answers/:value', answerController.execute);
 
 export default router;
